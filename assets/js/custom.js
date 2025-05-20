cc93f0b28363cc0801ff9b42f2ebfb5b320d49e7
dhwani <dhwaniturakhia25@gmail.com>
2025-05-20 09:53:43 +0530

    pages add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,11 +7,23 @@ jQuery(document).ready(function ($) {
     }
   });
 
+  $(".down-arrow").click(function (e) {
+    e.stopPropagation();
+    const parentLi = $(this).closest("li");
+
+    if (parentLi.hasClass("active")) {
+      parentLi.removeClass("active");
+    } else {
+      $(".navbar-menu li").removeClass("active");
+      parentLi.addClass("active");
+    }
+  });
+
   $(".mobile-toggle").click(function () {
     $("body").toggleClass("mobile-open");
   });
 
-  $(".navbar-menu a").click(function () {
+  $(".nav-links").click(function () {
     $("body").removeClass("mobile-open");
   });
 
@@ -25,37 +37,37 @@ jQuery(document).ready(function ($) {
     arrows: false,
   });
 
-    $('.serviceslider').slick({
+  $('.serviceslider').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: true,
     arrows: false,
-    autoplay:true,
+    autoplay: true,
     responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    ]
   });
 
 });
